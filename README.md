948a1bbcc797eef50f91d7f64759ee0459bea934
Pedro Henrique Alves Veloso <contatopedrohalves@gmail.com>
2023-09-09 20:25:51 -0300

    back/front

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 Na branch `main` está tudo feito durante o evento do Bootcamp, nessa ficará a continuação do desenvolvimento.
 
-O objetivo é continar o projeto, para o uso no meu TCC.
+O objetivo é continuar o projeto, para o uso no meu TCC.
--- frontend/src/ui/pages/ConfigPage.tsx
+++ frontend/src/ui/pages/ConfigPage.tsx
@@ -4,6 +4,10 @@ export default function ConfigPage() {
   return <>
     <Navbar />
 
+    <header>
+      <h1></h1>
+    </header>
+
     <main className="h-full w-full items-center justify-center flex">
       <h1 className="text-[2rem] text-gray-800 text-center">Ainda não implementado.</h1>
     </main>
